674609a3c8c1aae12be8a6e3c26af436f30d4854
OmprakashKhamari <omprakashkhamari28@gmail.com>
2024-03-21 14:20:45 +0530

    Assignment-1

--- Assignment-1/program-1.cpp
+++ Assignment-1/program-1.cpp
@@ -6,13 +6,13 @@ int main(){
    for (int i=1; i<=100; i++){
 
       if (i%15 == 0)
-         cout<<"Fizz Buzz,\t";
+         cout<<"Fizz Buzz";
       else if ((i%3) == 0)
-         cout<<"Fizz,\t";
+         cout<<"Fizz";
       else if ((i%5) == 0)
-         cout<<"Buzz,\t";
+         cout<<"Buzz";
       else
-         cout<<i<<",\t";
+         cout<<i<<",";
    }
    return 0;
 }
